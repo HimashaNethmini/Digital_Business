5c470ab5270f265681aea3e7526098979f4e4d2a
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-07 21:56:05 +0530

    created the navigation abr

---
+++ src/utils/components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+      <div className="container">
+        <div className="n-container">
+          {/*left side */}
+          <div className="n-logo">
+            <span>Digi Bus</span>
+          </div>
+
+          {/* right side */}
+          <div className="n-right">
+            <div className="n-menu">
+                {/* making the navigation bar titles */}
+              <span>what we do </span>
+              <span> how it works </span>
+              <span> who we invest in </span>
+              <span> Testimonials </span>
+            </div>
+
+            {/* button */}
+              <div className="fund-button">Get Funded</div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
