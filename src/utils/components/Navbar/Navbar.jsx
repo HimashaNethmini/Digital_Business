bb621cec6d9e306669cd74af8e1bd2dfa03afc84
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-10 12:03:36 +0530

    add mobile screen navbar

--- src/utils/components/Navbar/Navbar.jsx
+++ src/utils/components/Navbar/Navbar.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./Navbar.css";
+import { BiMenuAltRight } from "react-icons/bi";
 
 const Navbar = () => {
   return (
     <div className="n-wrapper">
-
       {/* desktop version */}
       <div className="container">
         <div className="n-container">
-
           {/*left side */}
           <div className="n-logo">
             <span>Digi Bus</span>
@@ -17,7 +16,7 @@ const Navbar = () => {
           {/* right side */}
           <div className="n-right">
             <div className="n-menu">
-                {/* making the navigation bar titles */}
+              {/* making the navigation bar titles */}
               <span>what we do </span>
               <span> how it works </span>
               <span> who we invest in </span>
@@ -25,16 +24,32 @@ const Navbar = () => {
             </div>
 
             {/* button */}
-              <div className="fund-button">Get Funded</div>
-            
+            <div className="fund-button">Get Funded</div>
           </div>
         </div>
       </div>
 
-
       {/* mobile/tab version */}
       <div className="nm-container">
-        mobile version
+        {/* logo */}
+        <span>Digi Bus</span>
+
+        {/*menu icon */}
+        <BiMenuAltRight size={40} />
+
+
+
+        {/*mobile menu */}
+        <div className="nm-menu">
+          <span>What we do</span>
+          <span>How it works</span>
+          <span>Who we invest in</span>
+          <span>Testimonials</span>
+
+        {/* button */}
+          <div className="m-funded-button">Get Funded </div>
+          
+        </div>
       </div>
     </div>
   );
