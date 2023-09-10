0c95a971fa3555cc903881027848c186f288915d
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-10 20:11:39 +0530

    add components

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Navbar from '@/src/utils/components/Navbar/Navbar';
 import './page.css'
+import Hero from '@/src/utils/components/Hero/Hero';
 export default function Home() {
 
   //homepage
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <div className="app">
       <Navbar />
+      <Hero />
     </div>
   );
 }
