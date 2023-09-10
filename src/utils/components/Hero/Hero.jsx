29a74d87934c255aee99b40d9d45e8cdb5d38634
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-10 20:37:39 +0530

    Make the text

---
+++ src/utils/components/Hero/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="h-wrapper">
+      <div className="container">
+        <div className="h-container">
+          {/*left side */}
+          <div className="h-left">Left</div>
+
+          {/*right side */}
+          <div className="h-right">
+            <div className="h-title">
+              <span>Redefine How</span>
+              <span>You Grow your</span>
+              <span>Digital Business</span>
+            </div>
+
+            <div className="h-des">
+              Revenue based funding and execution support designed for early
+              stage founders
+            </div>
+
+            {/* email box*/}
+            <span>Email</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
