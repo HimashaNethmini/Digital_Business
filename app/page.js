611cc2a9013690a8f9a6d3b1554ff81d3d3fc8ad
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-11 11:32:11 +0530

    recorrect the compo. path

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import Navbar from '@/src/utils/components/Navbar/Navbar';
+import Navbar from '@/src/components/Navbar/Navbar';
 import './page.css'
-import Hero from '@/src/utils/components/Hero/Hero';
-export default function Home() {
+import Hero from '@/src/components/Hero/Hero';
 
+export default function Home() {
   //homepage
-  
   return (
     <div className="app">
       <Navbar />
