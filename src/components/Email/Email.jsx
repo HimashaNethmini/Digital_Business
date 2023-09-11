10fc4c16610792929c2bb8c3e002b557376c0100
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-11 21:13:33 +0530

    add animation

--- src/components/Email/Email.jsx
+++ src/components/Email/Email.jsx
@@ -1,23 +1,61 @@
 import React from 'react'
 import './Email.css'
 import { LuMail } from "react-icons/lu";
+import { easeOut, motion } from 'framer-motion';
+import { containerVariants } from '@/src/utils/animations';
+
 const Email = () => {
+
+
   return (
-    <div className="emailBox">
+    <motion.div
+    initial={{
+      width: ".5rem",
+      borderRadius: "100%",
+    }} 
+    whileInView={{
+      width: "70%",
+      borderRadius: "999px",
+      transition: {
+        type: "easeOut",
+        duration: 1,
+      },
+    }}
+    className="emailBox">
     
     {/* icon */}
-    <div>
+    <motion.div
+    variants={containerVariants(0.6)}
+    initial="offscreen"
+    whileInView={"onscreen"}
+    viewport={{
+      once: true  /*run the animation when it's first time in the screen*/
+    }}>
+
         <LuMail size ={40} />
-    </div>
+    </motion.div>
 
     {/*input*/}
-    <input type="email" placeholder='Enter Email' />
+    <motion.input
+     variants={containerVariants(0.6)}
+     initial="offscreen"
+     whileInView={"onscreen"}
+     viewport={{
+       once: true  /*run the animation when it's first time in the screen*/
+     }}
+     type="email" placeholder='Enter Email' />
 
     {/*get funded button*/}
-    <div className="getFunded">
-        Get Funded
-    </div>
-    </div>
+    <motion.div
+    variants={containerVariants(0.6)}
+    initial="offscreen"
+    whileInView={"onscreen"}
+    viewport={{
+      once: true  /*run the animation when it's first time in the screen*/
+    }}
+    className="getFunded"> Get Funded</motion.div>
+
+    </motion.div>
   )
 }
 
