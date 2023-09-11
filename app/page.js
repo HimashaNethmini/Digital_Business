987c402f2e2b362442cd0bffce663e89acdaa672
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-11 21:27:30 +0530

    add compo.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Navbar from '@/src/components/Navbar/Navbar';
 import './page.css'
 import Hero from '@/src/components/Hero/Hero';
+import BrandingVideo from '@/src/components/BrandingVideo/BrandingVideo';
 
 export default function Home() {
   //homepage
@@ -8,6 +9,7 @@ export default function Home() {
     <div className="app">
       <Navbar />
       <Hero />
+      <BrandingVideo />
     </div>
   );
 }
