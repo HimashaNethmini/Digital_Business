af064bfe8fc4ad8887cf4ecb95c0e20c1dd2f281
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-13 10:34:23 +0530

    render animation from animation.js

--- src/components/Whatwedo/Whatwedo.jsx
+++ src/components/Whatwedo/Whatwedo.jsx
@@ -1,23 +1,41 @@
+"use client";
+
 import React from "react";
 import "./Whatwedo.css";
 import { features } from "@/src/utils/data";
 import Image from "next/image";
-
+import { motion } from "framer-motion";
+import { desVariants, tagVariants, titleVariants } from "@/src/utils/animations";
 
 const Whatwedo = () => {
   return (
     <div className="wwd-wrapper">
       <div className="container">
         <div className="wwd-container">
+
           {/* main texts */}
           <div className="wwd-head">
-            <span className="tag">What we do</span>
-            <span className="title">
+            <motion.span 
+            initial="offscreen"
+            whileInView={"onscreen"}
+            variants={tagVariants}
+            className="tag">What we do</motion.span>
+
+            <motion.span 
+            initial="offscreen"
+            whileInView={"onscreen"}
+            variants={titleVariants}
+            className="title">
               {" "}
               Emowering founders with non dilutive capital and excution
               expertise
-            </span>
-            <span className="desc">Here is how we can evaluate</span>
+            </motion.span>
+            
+            <motion.span
+            initial="offscreen"
+            whileInView={"onscreen"}
+            variants={desVariants}
+             className="desc">Here is how we can evaluate</motion.span>
           </div>
 
           {/* 2 block cards */}
@@ -69,8 +87,35 @@ const Whatwedo = () => {
                 </div>
             
             </div>
+          </div>
+
+          {/* supporting blocks */}
+          <div className="wwd-support">
+
+            {/* left side*/}
+            <div>
+              <span className="sec-title">Blue Growth Support</span>
+              <span className="desc">
+                Data Insights and full stack expertise to supercharge growth
+              </span>
+            </div>
+
+            {/*right side */}
+            <div>
+              <span className="text">
+                Actionable data insights by connecting revenue, marketing
+                social media platforms
+              </span>
+
+              <span className="text">
+                On demand exxecution expertise at cost or revenue share 
+                proposition design, engineering, marketing analytics,
+                partnerships, brands, intellectual property, market expansion talent management.
+              </span>
+            </div>
 
           </div>
+
         </div>
       </div>
     </div>
