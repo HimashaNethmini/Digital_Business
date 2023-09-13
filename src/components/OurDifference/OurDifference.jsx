92e8fd7ab2d0bfaffe5b821f7d1319f680b5b03b
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-13 11:21:02 +0530

    make the texts

--- src/components/OurDifference/OurDifference.jsx
+++ src/components/OurDifference/OurDifference.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./OurDifference.css";
 
 const OurDifference = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="od-wrapper">
+      <div className="container">
+        <div className="od-container">
+          <div className="od-head">
+            
+            {/* texts */}
+            <span className="tag"> Our Difference</span>
+            <span className="title">Fair capital, hassle free</span>
+            <span className="text">
+              Our mission is to level the playing field for early stage growth
+              in business capital
+              <br />
+              We provide capital that is unbiased, flexible and non dilutive
+              with the execution support to accelerate value creation
+            </span>
 
-export default OurDifference
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurDifference;
