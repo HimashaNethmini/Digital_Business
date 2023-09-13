617a86f1d7ba22a54210121c547277a3ebcd7ba5
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-13 19:41:04 +0530

    added animations

--- src/components/OurDifference/OurDifference.jsx
+++ src/components/OurDifference/OurDifference.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import "./OurDifference.css";
 import { ourDiffFeatures } from "@/src/utils/data";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { desVariants, tagVariants, titleVariants } from "@/src/utils/animations";
 
 const OurDifference = () => {
   return (
@@ -12,16 +14,30 @@ const OurDifference = () => {
         <div className="od-container">
           <div className="od-head">
 
-            {/* texts */}
-            <span className="tag"> Our Difference</span>
-            <span className="title">Fair capital, hassle free</span>
-            <span className="text">
+            {/* texts with animations*/}
+            <motion.span 
+            variants={tagVariants}
+            initial="offscreen"
+            whileInView={"onscreen"}
+            className="tag"> Our Difference</motion.span>
+
+            <motion.span
+            variants={titleVariants}
+            initial="offscreen"
+            whileInView={"onscreen"}
+            className="title">Fair capital, hassle free</motion.span>
+
+            <motion.span
+            variants={desVariants}
+            initial="offscreen"
+            whileInView={"onscreen"}
+            className="text">
               Our mission is to level the playing field for early stage growth
               in business capital
               <br />
               We provide capital that is unbiased, flexible and non dilutive
               with the execution support to accelerate value creation
-            </span>
+            </motion.span>
 
           </div>
 
