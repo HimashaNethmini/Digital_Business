ba3eaa1a31fb39b92a5f1b66e709015f37e58f11
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-13 19:22:19 +0530

    made feature cards

--- src/components/OurDifference/OurDifference.jsx
+++ src/components/OurDifference/OurDifference.jsx
@@ -1,5 +1,9 @@
+"use client";
+
 import React from "react";
 import "./OurDifference.css";
+import { ourDiffFeatures } from "@/src/utils/data";
+import Image from "next/image";
 
 const OurDifference = () => {
   return (
@@ -7,7 +11,7 @@ const OurDifference = () => {
       <div className="container">
         <div className="od-container">
           <div className="od-head">
-            
+
             {/* texts */}
             <span className="tag"> Our Difference</span>
             <span className="title">Fair capital, hassle free</span>
@@ -18,6 +22,28 @@ const OurDifference = () => {
               We provide capital that is unbiased, flexible and non dilutive
               with the execution support to accelerate value creation
             </span>
+
+          </div>
+
+          {/* features */}
+          <div className="od-features">
+
+            {
+              ourDiffFeatures.map((feature, i) => (
+                < div key={i} className="od-feature">
+                  <Image src ={feature.icon}
+                  alt="features"
+                  width={128}
+                  height={128} 
+                  />
+                <br />
+                  <span className="sec-title"> {feature.title}</span>
+                  <span className="text"><br />{feature.des}</span>
+
+                </div>
+
+              ))
+            }
 
           </div>
         </div>
