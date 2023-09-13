2e4669268cfc5ae9acd6a795568625bd48650e3a
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-13 11:07:39 +0530

    wrap ourDiff component with color change animation

--- app/page.js
+++ app/page.js
@@ -1,17 +1,43 @@
+"use client";
+
 import Navbar from '@/src/components/Navbar/Navbar';
 import './page.css'
 import Hero from '@/src/components/Hero/Hero';
 import BrandingVideo from '@/src/components/BrandingVideo/BrandingVideo';
 import Whatwedo from '@/src/components/Whatwedo/Whatwedo';
+import OurDifference from '@/src/components/OurDifference/OurDifference';
+import { motion, useAnimation } from 'framer-motion';
 
 export default function Home() {
+
+  const controls = useAnimation()
   //homepage
   return (
-    <div className="app">
+    <motion.div className="app" animate={controls}>
       <Navbar />
       <Hero />
       <BrandingVideo />
       <Whatwedo />
-    </div>
+      
+  {/*wrapping our difference page with animation to change colour smoothly */}
+      
+      <motion.div
+      onViewportEnter={() =>
+      controls.start({
+        backgroundColor: "var(--secondary-color)",
+      })
+      }
+      onViewportLeave={() =>
+      controls.start({
+        backgroundColor: "white",
+      })
+    }
+      viewport = {{amount:0.4}}
+      >
+        
+        <OurDifference />
+      </motion.div>
+      
+    </motion.div>
   );
 }
