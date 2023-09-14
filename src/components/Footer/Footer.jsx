d8a9b43e82b38c770c1c921ec94de07ce1278ea2
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-14 21:44:50 +0530

    develop the footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Footer.css";
+import Email from "../Email/Email";
+const Footer = () => {
+  return (
+    <div className="f-wrapper">
+      <div className="container">
+        <div className="f-container">
+          <span className="title">Get Funded Today ! </span>
+          <Email />
+
+          <hr />
+
+          {/*menu */}
+          <div className="f-menu">
+            <span>Home</span>
+            <span>What we do</span>
+            <span>How it works</span>
+            <span>Who we invest in</span>
+            <span>Testimonials</span>
+          </div>
+        <hr />
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
