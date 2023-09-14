99fd8c1a51c9d28cc350e5f31f0820ff93c0c553
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-14 11:39:53 +0530

    development completed

--- src/components/Whoweinvest/Whoweinvest.jsx
+++ src/components/Whoweinvest/Whoweinvest.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Whoweinvest.css";
 import { whoWeInvest } from "@/src/utils/data";
 import image from "@/public/persons.png";
+import { motion } from "framer-motion";
+import { tagVariants } from "@/src/utils/animations";
 
 const Whoweinvest = () => {
   return (
@@ -11,17 +13,26 @@ const Whoweinvest = () => {
           {/* left side */}
           <div className="ww-left">
             <div className="head">
-              <span className="tag">Who we invest in</span>
-              <span className="title">
+              <motion.span
+              variants={tagVariants}
+              initial="offscreen"
+              whileInView={"onscreen"} 
+              className="tag">Who we invest in</motion.span>
+
+              <motion.span 
+              variants={tagVariants}
+              initial="offscreen"
+              whileInView={"onscreen"}
+              className="title">
                 Digital businesses <br /> with early transaction
-              </span>
+              </motion.span>
             </div>
 
             {/* features */}
             <div className="ww-features">
               {whoWeInvest.map((feature, i) => (
                 <div key={i} className="ww-feature">
-                  <span className="des"> {feature.title}</span>
+                  <span className="des"> {feature.title} <br /> </span>
                   <span className="text">{feature.des}</span>
                 </div>
               ))}
