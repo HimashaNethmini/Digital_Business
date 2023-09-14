e6ca0d981ef196f766d024a6a1214ab7076e7793
HimashaNethmini <himashanethmini05@gmail.com>
2023-09-14 21:27:46 +0530

    develop the testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Testimonials.css";
-import { motion } from "framer-motion";
+//import { motion } from "framer-motion";
 import { tagVariants, titleVariants } from "@/src/utils/animations";
-import { testimonialsData } from "@/src/utils/data";
+import SlickSlider from "./SlickSlider";
 
 const Testimonials = () => {
   return (
@@ -11,33 +11,32 @@ const Testimonials = () => {
         <div className="t-container">
           {/* text with animations */}
           <div className="t-head">
-            <motion.span
-            variants={tagVariants}
-            initial="offscreen"
-            whileInView={"onscreen"} 
-            className="tag">Testimonals</motion.span>
-
-            <motion.span 
-            variants={titleVariants}
-            initial="offscreen"
-            whileInView={"onscreen"}
-            className="title">
-                Access capital that complements traditional funding options
+            {/*<motion.span
+              variants={tagVariants}
+              initial="offscreen"
+              whileInView={"onscreen"}
+              className="tag"
+            >
+              Testimonals
             </motion.span>
+
+            <motion.span
+              variants={titleVariants}
+              initial="offscreen"
+              whileInView={"onscreen"}
+              className="title"
+            >
+              Access capital that complements traditional funding options
+  </motion.span> */}
+            <span className="tag">Testimonals</span>
+            <span className="title">Access capital that complements traditional funding options</span>
 
             <span className="des">What people say about us</span>
           </div>
-
-          {/* slider*/}
-          <div className="t-slider">
-            {
-                testimonialsData.map((feature, i) => (
-                    <div key={i}>
-                    </div>
-                ))
-            }
-          </div>
         </div>
+
+        {/* slider*/}
+        <SlickSlider />
       </div>
     </div>
   );
